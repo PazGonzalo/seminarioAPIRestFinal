76cfb95c786a13217834201a412c377916a17b25
Juan <iviglia.j@hotmail.com>
2021-10-18 17:54:14 -0300

    Actualizacion Etiquetas

--- src/main/java/org/grupo2/seminarioAPIRestPrueba/controladoresRest/ControladorRestEtiquetas.java
+++ src/main/java/org/grupo2/seminarioAPIRestPrueba/controladoresRest/ControladorRestEtiquetas.java
@@ -3,13 +3,11 @@ package org.grupo2.seminarioAPIRestPrueba.controladoresRest;
 import org.grupo2.controladores.ControladorEtiquetas;
 import org.grupo2.exceptions.EtiquetaException;
 import org.grupo2.vo.EtiquetaVO;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class ControladorRestEtiquetas {
 
     @PostMapping("/crearEtiquetas")
