9e7616dfdd464c6b474fc20dd468edf2a30b1c92
Juan <iviglia.j@hotmail.com>
2021-10-26 10:26:46 -0300

    Correciones by Pampix

--- src/main/java/org/grupo2/seminarioAPIRestPrueba/controladoresRest/ControladorRestEtiquetas.java
+++ src/main/java/org/grupo2/seminarioAPIRestPrueba/controladoresRest/ControladorRestEtiquetas.java
@@ -11,13 +11,13 @@ import java.util.List;
 public class ControladorRestEtiquetas {
 
     @PostMapping("/crearEtiquetas")
-    @CrossOrigin(origins="https://localhost:3000/")
+    @CrossOrigin(origins="http://localhost:3000/")
     public void crearEtiquetas(@RequestParam(name = "descripcion") String descripcion) throws EtiquetaException {
         ControladorEtiquetas.getInstance().crearEtiqueta(descripcion);
     }
 
     @RequestMapping("/getEtiquetas")
-    @CrossOrigin(origins="https://localhost:3000/")
+    @CrossOrigin(origins="http://localhost:3000/")
     public List<EtiquetaVO> getEtiquetas() throws EtiquetaException{
         return ControladorEtiquetas.getInstance().obtenerAllEtiquetas();
     }
