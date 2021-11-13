a9d7c952aabf815a8e0e348be38681c280be4bd6
Juan <iviglia.j@hotmail.com>
2021-11-13 17:10:58 -0300

    Array de fotos - falta agregarlo bien en el test

---
+++ src/main/java/org/grupo2/seminarioAPIRestPrueba/controladoresRest/ControladorRestGalerias.java
@@ -0,0 +1,31 @@
+package org.grupo2.seminarioAPIRestPrueba.controladoresRest;
+
+import org.grupo2.controladores.ControladorGalerias;
+import org.grupo2.exceptions.GaleriaException;
+import org.grupo2.exceptions.UniversidadException;
+import org.grupo2.modelo.Galeria;
+import org.grupo2.vo.GaleriaVO;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ControladorRestGalerias {
+
+    @PostMapping("/crearFoto")
+    @CrossOrigin(origins = "http://localhost:3000/")
+    public void crearFoto(@RequestParam(name = "nombre") String nombre,
+                          @RequestParam(name = "archivo") String archivo,
+                          @RequestParam(name = "idUniversidad") int idUniversidad) throws GaleriaException, UniversidadException {
+        ControladorGalerias.getInstance().crearFotoUniversidad(nombre,archivo,idUniversidad);
+    }
+    @PostMapping("/obtenerFotosPorUniversidad")
+    @CrossOrigin(origins = "http://localhost:3000/")
+    public List<GaleriaVO> obtenerFotosPorUniversidad(int idUniversidad) throws GaleriaException {
+        return ControladorGalerias.getInstance().obtenerFotosPorUniversidad(idUniversidad);
+    }
+
+}
