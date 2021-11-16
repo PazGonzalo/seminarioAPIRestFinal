451ec813ef02282a9f3bc51a879e00bfdeeb80d7
PazGonzalo <89784467+PazGonzalo@users.noreply.github.com>
2021-11-16 16:45:56 -0300

    Eventos patrocinados agregados

---
+++ src/main/java/org/grupo2/seminarioAPIRestPrueba/controladoresRest/ControladorRestEventos.java
@@ -0,0 +1,46 @@
+package org.grupo2.seminarioAPIRestPrueba.controladoresRest;
+
+import org.grupo2.controladores.ControladorEventos;
+import org.grupo2.exceptions.EventoException;
+import org.grupo2.exceptions.UniversidadException;
+import org.grupo2.vo.EventoVO;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+public class ControladorRestEventos {
+
+    @PostMapping("/crearEvento")
+    @CrossOrigin(origins="http://localhost:3000/")
+    public void crearEvento(@RequestParam(name = "nombre") String nombre,
+                            @RequestParam(name = "descripcion") String descripcion,
+                            @RequestParam(name = "fecha") String fecha,
+                            @RequestParam(name = "hora") String hora,
+                            @RequestParam(name = "idUniversidad") int idUniversidad) throws UniversidadException, EventoException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate fechaFinal = LocalDate.parse(fecha, formatter);
+        ControladorEventos.getInstancia().crearEvento(nombre, descripcion, fechaFinal, hora, idUniversidad);
+    }
+
+    @RequestMapping("/getAllEventos")
+    @CrossOrigin(origins="http://localhost:3000/")
+    public List<EventoVO> getAllEventos() throws EventoException {
+        return ControladorEventos.getInstancia().getAllEventos();
+    }
+
+    @RequestMapping("/getEventosByUniversidad")
+    @CrossOrigin(origins="http://localhost:3000/")
+    public List<EventoVO> getEventosByUniversidad(@RequestParam(name = "idUniversidad") int idUniversidad) throws EventoException {
+        return ControladorEventos.getInstancia().getEventosByUniversidad(idUniversidad);
+    }
+
+    @RequestMapping("/getEventoById")
+    @CrossOrigin(origins="http://localhost:3000/")
+    public EventoVO getEventoById(@RequestParam(name = "idEvento") int idEvento) throws EventoException {
+        return ControladorEventos.getInstancia().getEventoById(idEvento);
+    }
+
+}
